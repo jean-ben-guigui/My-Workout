37c57e36b04bd4822a04cc00f37bb2a253a91700
jean-ben-guigui <duver.arthur@gmail.com>
2020-04-27 00:35:43 +0200

    better overall layout

--- My Workout/Extensions/String+HtmlHelper.swift
+++ My Workout/Extensions/String+HtmlHelper.swift
@@ -14,7 +14,7 @@ extension String {
         guard let data = data(using: .utf8) else { return NSAttributedString() }
         do {
             let string = try NSMutableAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding:String.Encoding.utf8.rawValue], documentAttributes: nil)
-            string.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor.label, range: NSRange(location: 0, length: string.length))
+                string.addAttributes([NSAttributedString.Key.foregroundColor: UIColor.label, NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: UIFont.TextStyle.body)], range: NSRange(location: 0, length: string.length))
             return string
         } catch {
             return NSAttributedString()
